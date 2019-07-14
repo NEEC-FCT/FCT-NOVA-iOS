8746eac1d6c2b388ed1b7d462c88afe29e331d7e
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-07-14 12:09:08 +0100

    Noticias feito mas com Bug no next separador

--- RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/Noticias.swift
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/Noticias.swift
@@ -12,10 +12,10 @@ import WebKit
 class Noticias: UIViewController  , WKNavigationDelegate {
     
     @IBOutlet weak var webview: WKWebView!
-    
+    var back:Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.back = false
         webview.isHidden = true
         let url = URL (string: "https://www.fct.unl.pt/noticias")
         let requestObj = URLRequest(url: url!)
@@ -26,8 +26,35 @@ class Noticias: UIViewController  , WKNavigationDelegate {
         
     }
     
+   
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+         webview.isHidden = true
+        if let urlStr = navigationAction.request.url?.absoluteString {
+            //urlStr is what you want
+            print(urlStr)
+            if(urlStr.hasPrefix("https://www.fct.unl.pt/noticias/")){
+                
+                self.back = true
+                guard let url = URL(string: urlStr) else { return }
+                UIApplication.shared.open(url)
+             
+    
+            }
+            
+            
+        }
+        decisionHandler(.allow)
+    }
+    
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 
+        
+        
+        if(self.back){
+            self.back = false
+            webview.goBack()
+        }
         webview.evaluateJavaScript("document.getElementById(\"sliding-popup\").style.display=\"none\";"
             + "document.getElementById('header3').style.display='none';"
             + "document.getElementById('content_top').style.display='none';"
@@ -35,15 +62,15 @@ class Noticias: UIViewController  , WKNavigationDelegate {
             + "document.getElementById('header1').style.display='none';"
             + "document.getElementById('breadcrumb').style.display='none';"
             + "document.getElementById('footer').style.display='none';", completionHandler: nil)
-      
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+        
+        
+       
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
             self.webview.isHidden = false
         }
     }
     
-    
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
