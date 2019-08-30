f5cc26019deb2b9a8d00bcc3df163ebeec05e01c
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-08-31 00:32:42 +0100

    Obtem HTML

--- RAMAnimatedTabBarController/ClipLogin.swift
+++ RAMAnimatedTabBarController/ClipLogin.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Foundation
+import Alamofire
+
 
 class ClipLogin: UIViewController  {
     
@@ -19,14 +21,40 @@ class ClipLogin: UIViewController  {
     
     @IBAction func loginClicked(_ sender: Any) {
         
-        print( logintext.text as! String)
-        print( passwordtext.text as! String)
+        print( logintext.text!)
+        print( passwordtext.text!)
+   
+        
+
+        
+        let params = ["identificador":"jm.veloso",
+                      "senha":"3wdvjcffez"
+                ]
+        
+        
+        ApiService.callPost(url: URL(string: "https://clip.unl.pt/utente/eu")!, params: params, finish: finishPost)
+
+
+       
+        
     }
     
     
+    
+    func finishPost (message:String, data:Data?) -> Void
+    {
+        do
+        {
+            print(message)
+            print("-------")
+            print( String(decoding: data!, as: UTF8.self) )
+  
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround() 
+        self.hideKeyboardWhenTappedAround()
         print("Go Clip Login")
         
       
@@ -51,5 +79,68 @@ extension UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+}
+
+
+extension Dictionary {
+    func percentEscaped() -> String {
+        return map { (key, value) in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+            }
+            .joined(separator: "&")
+    }
+}
+
+extension CharacterSet {
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+        
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+}
+
+
+class ApiService
+{
+    static func getPostString(params:[String:Any]) -> String
+    {
+        var data = [String]()
+        for(key, value) in params
+        {
+            data.append(key + "=\(value)")
+        }
+        return data.map { String($0) }.joined(separator: "&")
+    }
+    
+    static func callPost(url:URL, params:[String:Any], finish: @escaping ((message:String, data:Data?)) -> Void)
+    {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let postString = self.getPostString(params: params)
+        request.httpBody = postString.data(using: .utf8)
+        
+        var result:(message:String, data:Data?) = (message: "Fail", data: nil)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if(error != nil)
+            {
+                result.message = "Fail Error not null : \(error.debugDescription)"
+            }
+            else
+            {
+                result.message = "Success"
+                result.data = data
+            }
+            
+            finish(result)
+        }
+        task.resume()
     }
 }
