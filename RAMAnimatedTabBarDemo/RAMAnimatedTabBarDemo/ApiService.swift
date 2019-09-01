eae9427b348c7ba4736d559452ad6067ff403272
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-09-01 23:06:06 +0100

    Obtem HTML do horário

--- RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/ApiService.swift
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/ApiService.swift
@@ -12,6 +12,54 @@ import SwiftSoup
 
 class ApiService
 {
+    
+
+    
+    static func callGetHorario(year:String,  studentNumberId:String ,  semester:Int , finish: @escaping ((message:String, data:Data?)) -> Void)
+    {
+     
+        let start = year.prefix(2)
+        let end = year.suffix(2)
+        let newyear = start + end
+        print("ano " + newyear)
+        let STUDENT_SCHEDULE_1 = "https://clip.unl.pt/utente/eu/aluno/ano_lectivo/hor%E1rio?" +
+        "ano_lectivo=";
+        let STUDENT_SCHEDULE_2 = "&institui%E7%E3o=97747&aluno=";
+        let STUDENT_SCHEDULE_3 = "&tipo_de_per%EDodo_lectivo=s&per%EDodo_lectivo=";
+        let STUDENT_SCHEDULE_3_TRIMESTER = "&tipo_de_per%EDodo_lectivo=t&per%EDodo_lectivo=";
+        
+        var url = STUDENT_SCHEDULE_1 + newyear + STUDENT_SCHEDULE_2 + studentNumberId;
+        if (semester == 3){
+            url += STUDENT_SCHEDULE_3_TRIMESTER + String(semester - 1);
+        } // Trimester
+        else{
+            url += STUDENT_SCHEDULE_3 + String(semester);
+        }
+
+        print(url)
+        
+        var request = URLRequest(url: NSURL(string: url)! as URL)
+        let session = URLSession.shared
+        request.httpMethod = "GET"
+        let cookies = readCookie(forURL: URL(string: "https://clip.unl.pt/utente/eu")!)
+        print("Cookies before request: ", cookies)
+        
+        var result:(message:String, data:Data?) = (message: "Fail", data: nil)
+        let task = session.dataTask(with: request) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse, let fields = httpResponse.allHeaderFields as? [String : String] {
+                let cookies = HTTPCookie.cookies(withResponseHeaderFields: fields, for: response!.url!)
+                HTTPCookieStorage.shared.setCookies(cookies, for: response!.url!, mainDocumentURL: nil)
+                result.message = "False"
+                result.data = data
+                
+            }
+            finish(result)
+        }
+        task.resume()
+        
+    }
+    
+    
     static func getPostString(params:[String:Any]) -> String
     {
         var data = [String]()
--- RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/showHorario.swift
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/showHorario.swift
@@ -14,16 +14,53 @@ import SwiftSoup
 
 class showHorario: UIViewController  {
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
         print("Show horario")
+           ApiService.callGetHorario(year: "2019/20", studentNumberId: "88508", semester: 1, finish: finishGetHorario)
         //gotoID
     
         
     }
     
+    
+    func finishGetHorario(message:String, data:Data?) -> Void
+    {
+        do
+        {
+            print(message)
+            print("----Horario---")
+            
+            let html = String(decoding: data!, as: UTF8.self)
+            print( html )
+            /*
+            do {
+                let doc: Document = try SwiftSoup.parse(html)
+                let links: Array<Element> = try doc.select("a[href]").array()
+                for link in links {
+                    let linkHref = try link.attr("href")
+                    let matched = matches(for: "/utente/eu/aluno/ano_lectivo[?][_a-zA-Z0-9=;&.%]*ano_lectivo=[0-9]*", in: linkHref)
+                    if( matched != []){
+                        let year = try link.text()
+                        print(year)
+                    }
+                    
+                }
+                
+            }  catch {
+                print("error")
+            }
+            
+            /*
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "gotohorario", sender: nil)
+            }*/
+            */
+            
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
