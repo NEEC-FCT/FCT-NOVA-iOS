47bdc6179e6229f381a3db7768d238c0b9b62ec3
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-09-02 00:26:50 +0100

    Obtem algumas informações do horario do clip

--- RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/showHorario.swift
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/showHorario.swift
@@ -35,16 +35,126 @@ class showHorario: UIViewController  {
             
             let html = String(decoding: data!, as: UTF8.self)
             print( html )
-            /*
+            
             do {
                 let doc: Document = try SwiftSoup.parse(html)
-                let links: Array<Element> = try doc.select("a[href]").array()
-                for link in links {
-                    let linkHref = try link.attr("href")
-                    let matched = matches(for: "/utente/eu/aluno/ano_lectivo[?][_a-zA-Z0-9=;&.%]*ano_lectivo=[0-9]*", in: linkHref)
-                    if( matched != []){
-                        let year = try link.text()
-                        print(year)
+                let trs: Array<Element> = try doc.select("tr[valign=center]").array()
+                for tr in trs {
+                    
+                    let tds = try tr.select("td[class~=turno.* celulaDeCalendario]");
+                    
+                    for td in tds {
+                        let child  = td.child(0).children();
+            
+                        var href = try child.get(2).attr("href").split(separator: "&");
+                        let dia = href[9];
+                        let turno = href[7].uppercased();
+                        
+                        var horas_inicio = tr.child(0);
+                        let horas_fim = tr.child(1);
+                        
+                        var scheduleDayNumber:Int = -1
+                        let stringArray = String(dia).components(separatedBy: CharacterSet.decimalDigits.inverted)
+                        for item in stringArray {
+                            if let number = Int(item) {
+                               scheduleDayNumber = number
+                                print("number: \(number)")
+                                break
+                            }
+                        }
+                        
+                
+                          //  Character.getNumericValue(dia.charAt(dia.length() - 1));
+                      
+                        var scheduleClassType = turno.suffix(5);
+                        scheduleClassType += href[8].suffix(href[8].count - 1);
+                        
+                        var scheduleClassName = try td.attr("title");
+                        var scheduleClassNameMin = child.get(0);
+                        
+                        
+          
+                   
+                        
+                        
+                        var scheduleClassRoom:String? = nil ;
+                        
+                        
+                        if (child.size() > 4){
+                            scheduleClassRoom = child.get(4).data();
+                        }
+                        
+                        
+                        var scheduleClassDuration = try td.attr("rowspan");
+                        
+                        // Calculate scheduleClassHourStart & End
+                        var scheduleClassHourStart:String? = nil;
+                        var scheduleClassHourEnd :String? = nil;
+                        
+                        
+                        
+                        do {
+                            
+                            
+                        let format1 = DateFormatter()
+                        format1.dateFormat = "k:mm"
+            
+                            let dateDuration = ( Int(scheduleClassDuration)! / 2);
+                        
+                            if (try horas_fim.text().count == 1) {
+                        // Start hour
+                                /*
+                            scheduleClassHourStart = try horas_inicio.text();
+                                let dateStart = format1.date(from: scheduleClassHourStart!);
+                        
+                                let calendar1 = NSCalendar.current.date(from: dateStart)
+                              
+                        //calendar1.setTime(dateStart);
+                                calendar1.date(from: dateStart)
+                                
+             
+                        calendar1.add(Calendar.HOUR, dateDuration);
+                        
+                        // End hour
+                        scheduleClassHourEnd = calendar1.get(Calendar.HOUR_OF_DAY) + ":" + calendar1.get(Calendar.MINUTE) + "0";*/
+                        } else {
+                        // Calculate start hour
+                                /*
+                                let dateStart = format1.date(from: horas_fim.text());
+                        
+                        // Subtract 30 minutes to the start hour
+                                let calendar1 = NSCalendar.current
+                        calendar1.setTime(dateStart);
+                        calendar1.add(Calendar.MINUTE, -30);
+                        
+                        scheduleClassHourStart = calendar1.get(Calendar.HOUR_OF_DAY) + ":" + calendar1.get(Calendar.MINUTE);
+                        
+                        // Calculate end hour
+                      //  calendar1.add(Calendar.HOUR_OF_DAY, dateDuration);
+                        Calendar.current.date(byAdding: .day, value: dateDuration, to: calendar1)
+                        
+                        scheduleClassHourEnd = calendar1.get(Calendar.HOUR_OF_DAY) + ":" + calendar1.get(Calendar.MINUTE);*/
+                        }
+                        
+                        } catch {
+                            print("error 2 catch")
+                        }
+                        
+                        
+                        
+                        
+                        // Create scheduleClass
+                        print("Dia semana " + String(scheduleDayNumber))
+                        print(scheduleClassName);
+                        print(scheduleClassNameMin);
+                        print(scheduleClassType);
+                        print(scheduleClassHourStart);
+                        print(scheduleClassHourEnd);
+                        print(scheduleClassRoom);
+                        
+                      
+                        
+                        
                     }
                     
                 }
@@ -53,11 +163,6 @@ class showHorario: UIViewController  {
                 print("error")
             }
             
-            /*
-            DispatchQueue.main.async {
-                self.performSegue(withIdentifier: "gotohorario", sender: nil)
-            }*/
-            */
             
         }
     }
