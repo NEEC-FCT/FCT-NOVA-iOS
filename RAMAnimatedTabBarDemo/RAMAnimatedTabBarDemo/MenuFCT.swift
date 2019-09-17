1cf81350734067df461f6ce8b4ede3ac9fb40e05
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-09-17 05:27:19 +0100

    Listeners do Menu feitos

--- RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/MenuFCT.swift
+++ RAMAnimatedTabBarDemo/RAMAnimatedTabBarDemo/MenuFCT.swift
@@ -24,10 +24,80 @@ class MenuFCT: UIViewController  {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func powerbyClicked(_ sender: Any) {
+        guard let url = URL(string: "http://neec-fct.com/") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    
+    @IBAction func neeclogoClicked(_ sender: Any) {
+        guard let url = URL(string: "http://neec-fct.com/") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    
+    @IBAction func propinasClicked(_ sender: Any) {
+        let alert = UIAlertController(title: "Brevemente", message: "Estamos a desenvolver esta funcionalidade, obrigado pela compreensão", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                print("default")
+                
+            case .cancel:
+                print("cancel")
+                
+            case .destructive:
+                print("destructive")
+                
+                
+            }}))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sobreClicked(_ sender: Any) {
+        guard let url = URL(string: "http://fctapp.neec-fct.com/equipa/about.html") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func happyClicked(_ sender: Any) {
+    }
+    
+    @IBAction func ecoClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func infoClicked(_ sender: Any) {
+    }
+    
+    @IBAction func mapaClicked(_ sender: Any) {
+    }
+    
+    @IBAction func calenClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func avaliacaoClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func clipClicked(_ sender: Any) {
+    }
+    
+    
+    @IBAction func horariocliked(_ sender: Any) {
+    }
+    
+    
     @IBAction func faceClicked(_ sender: Any) {
+        guard let url = URL(string: "https://www.facebook.com/fct.nova/") else { return }
+        UIApplication.shared.open(url)
     }
     
     @IBAction func instaClicked(_ sender: Any) {
+        guard let url = URL(string: "https://www.instagram.com/fctnova/") else { return }
+        UIApplication.shared.open(url)
     }
     
     
